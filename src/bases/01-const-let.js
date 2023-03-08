c266fbc56ec1ae387cce82d9615e57bfaa982f29
rexdaro <rexdaro@gmail.com>
2023-03-08 09:54:30 -0300

    first commit

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+// variables y constantes
+
+const nombre = 'Manuel';
+let apellido = 'Rivas';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+    const nombre = 'peter';
+    let valorDado = 6;
+    console.log(nombre);
+}
+
+console.log(valorDado);
